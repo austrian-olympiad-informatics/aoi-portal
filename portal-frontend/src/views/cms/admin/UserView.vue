32b5c061b70abbcf274d5fcd2afcd07410f5d607
Otto Winter <otto@otto-winter.com>
2022-11-24 17:07:47 +0100

    Show register info on cms admin page

--- portal-frontend/src/views/cms/admin/UserView.vue
+++ portal-frontend/src/views/cms/admin/UserView.vue
@@ -16,13 +16,17 @@
       <h1 class="title is-2">Admin - User {{ fullName }}</h1>
       <div class="block" v-if="submissions !== null">
         <h2 class="title is-4">Submissions</h2>
-        <router-link :to="{ name: 'CMSAdminSubmissions', query: { user_id: userId } }">
+        <router-link
+          :to="{ name: 'CMSAdminSubmissions', query: { user_id: userId } }"
+        >
           {{ submissions.total }} submissions
         </router-link>
       </div>
       <div class="block" v-if="userEvals !== null">
         <h2 class="title is-4">User Evals</h2>
-        <router-link :to="{ name: 'CMSAdminUserEvals', query: { user_id: userId } }">
+        <router-link
+          :to="{ name: 'CMSAdminUserEvals', query: { user_id: userId } }"
+        >
           {{ userEvals.total }} user evals
         </router-link>
       </div>
@@ -35,12 +39,60 @@
           <li>Username: {{ user.username }}</li>
         </ul>
       </div>
+      <div class="block" v-if="registerInfo !== null">
+        <h2 class="title is-4">Register Info</h2>
+        <b>Vorname:</b> {{ registerInfo.first_name }} <br />
+        <b>Nachname:</b> {{ registerInfo.last_name }} <br />
+        <b>Email:</b>
+        <a :href="`mailto:${registerInfo.email}`"> {{ registerInfo.email }} </a>
+        <br />
+        <b>Adresse:</b>
+        {{ registerInfo.address_street ? registerInfo.address_street : "N/A" }}
+        <br />
+        <b>Stadt:</b>
+        {{ registerInfo.address_town ? registerInfo.address_town : "N/A" }}
+        <br />
+        <b>PLZ:</b>
+        {{ registerInfo.address_zip ? registerInfo.address_zip : "N/A" }}
+        <br />
+        <b>Geburtstag:</b>
+        {{ registerInfo.birthday ? registerInfo.birthday : "N/A" }} <br />
+        <b>CMS Username:</b>
+        {{
+          registerInfo.cms_username
+            ? `${registerInfo.cms_username} (#${registerInfo.cms_id})`
+            : "N/A"
+        }}
+        <br />
+        <b>Erstellt am:</b> {{ registerInfo.created_at }} <br />
+        <b>Admin?</b> {{ registerInfo.is_admin }} <br />
+        <b>Telefonnummer:</b>
+        {{ registerInfo.phone_nr ? registerInfo.phone_nr : "N/A" }} <br />
+        <b>Schule:</b>
+        {{ registerInfo.school_name ? registerInfo.school_name : "N/A" }} <br />
+        <b>Schuladresse:</b>
+        {{ registerInfo.school_address ? registerInfo.school_address : "N/A" }}
+        <br />
+        <div class="buttons is-pulled-right">
+          <b-button
+            tag="router-link"
+            :to="{ name: 'AdminUser', params: { userId: registerInfo.id } }"
+            icon-left="pencil"
+            type="is-warning"
+          >
+            Edit
+          </b-button>
+        </div>
+      </div>
       <div class="block">
         <h2 class="title is-4">Participations</h2>
         <ul>
           <li v-for="part in user.participations" :key="part.id">
             <router-link
-              :to="{ name: 'CMSAdminContest', params: { contestId: part.contest.id }}"
+              :to="{
+                name: 'CMSAdminContest',
+                params: { contestId: part.contest.id },
+              }"
             >
               {{ part.contest.name }}
             </router-link>
@@ -53,8 +105,13 @@
 
 <script lang="ts">
 import cmsadmin from "@/services/cmsadmin";
-import { AdminUser, AdminContestParticipations, AdminSubmissionsPaginated, AdminUserEvalsPaginated } from "@/types/cmsadmin";
-import { formatDateShort } from "@/util/dt";
+import admin from "@/services/admin";
+import {
+  AdminUser,
+  AdminSubmissionsPaginated,
+  AdminUserEvalsPaginated,
+} from "@/types/cmsadmin";
+import { AdminUser as AdminRegisterUser } from "@/types/admin";
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
@@ -65,6 +122,7 @@ export default class AdminUserView extends Vue {
   user: AdminUser | null = null;
   submissions: AdminSubmissionsPaginated | null = null;
   userEvals: AdminUserEvalsPaginated | null = null;
+  registerInfo: AdminRegisterUser | null = null;
 
   get fullName() {
     if (this.user === null) return null;
@@ -86,8 +144,22 @@ export default class AdminUserView extends Vue {
       perPage: 0,
     });
   }
+  async loadRegisterData() {
+    const users = await admin.getUsers();
+    for (const user of users) {
+      if (user.cms_id === this.userId) {
+        this.registerInfo = user;
+        return;
+      }
+    }
+  }
   async mounted() {
-    await Promise.all([this.loadUser(), this.loadSubmissions(), this.loadUserEvals()]);
+    await Promise.all([
+      this.loadUser(),
+      this.loadSubmissions(),
+      this.loadUserEvals(),
+      this.loadRegisterData(),
+    ]);
   }
 }
 </script>
